bfabdeed4f63340fe0450d8d4e170db97fd474cf
toyboot4e <toyboot4e@gmail.com>
2022-03-28 20:00:03 +0900

    add owned versions of `*ArgSystem`

--- src/sys/owned.rs
+++ src/sys/owned.rs
@@ -3,7 +3,7 @@
 use std::fmt;
 
 use crate::{
-    sys::{AccessSet, ExclusiveSystem, System},
+    sys::{AccessSet, ArgSystem, ExclusiveArgSystem, ExclusiveSystem, System},
     world::borrow::GatBorrowWorld,
     World,
 };
@@ -55,6 +55,53 @@ pub trait IntoExclusiveBoxSystem<Params, Ret> {
     fn into_ex_box_system(self) -> ExclusiveBoxSystem<Ret>;
 }
 
+/// Owned arg system
+pub struct BoxArgSystem<Data, Ret> {
+    f: Box<dyn for<'w> FnMut(Data, &'w World) -> Ret>,
+    accesses: AccessSet,
+}
+
+impl<Data, Ret> fmt::Debug for BoxArgSystem<Data, Ret> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "BoxArgSystem")
+    }
+}
+
+impl<Data, Ret> BoxArgSystem<Data, Ret> {
+    pub fn run_arg(&mut self, data: Data, world: &World) -> Ret {
+        (self.f)(data, world)
+    }
+
+    pub fn accesses(&self) -> &AccessSet {
+        &self.accesses
+    }
+}
+
+/// Owned exclusive arg system
+pub trait IntoBoxArgSystem<Data, Params, Ret> {
+    fn into_box_arg_system(self) -> BoxArgSystem<Data, Ret>;
+}
+
+pub struct ExclusiveBoxArgSystem<Data, Ret> {
+    f: Box<dyn for<'w> FnMut(Data, &'w mut World) -> Ret>,
+}
+
+impl<Data, Ret> fmt::Debug for ExclusiveBoxArgSystem<Data, Ret> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "ExclusiveBoxArgSystem")
+    }
+}
+
+impl<Data, Ret> ExclusiveBoxArgSystem<Data, Ret> {
+    pub fn run_arg_ex(&mut self, data: Data, world: &mut World) -> Ret {
+        (self.f)(data, world)
+    }
+}
+
+pub trait IntoExclusiveBoxArgSystem<Data, Params, Ret> {
+    fn into_ex_box_arg_system(self) -> ExclusiveBoxArgSystem<Data, Ret>;
+}
+
 macro_rules! impl_into_system {
     ($($xs:ident),*) => {
         impl<S, $($xs),*, Ret> IntoBoxSystem<($($xs,)*), Ret> for S
@@ -91,6 +138,41 @@ macro_rules! impl_into_system {
                 }
             }
         }
+
+        impl<S, Data, $($xs),*, Ret> IntoBoxArgSystem<Data, ($($xs,)*), Ret> for S
+        where
+            S: ArgSystem<Data, ($($xs,)*), Ret> + 'static,
+            $($xs: GatBorrowWorld,)*
+        {
+            fn into_box_arg_system(mut self) -> BoxArgSystem<Data, Ret> {
+                let accesses = S::accesses(&self);
+
+                let f = Box::new(move |data: Data, world: &World| unsafe {
+                     self.run_arg(data, world)
+                });
+
+                BoxArgSystem {
+                    f,
+                    accesses,
+                }
+            }
+        }
+
+        impl<S, Data, $($xs),*, Ret> IntoExclusiveBoxArgSystem<Data, ($($xs,)*), Ret> for S
+        where
+            S: ExclusiveArgSystem<Data, ($($xs,)*), Ret> + 'static,
+            $($xs: GatBorrowWorld,)*
+        {
+            fn into_ex_box_arg_system(mut self) -> ExclusiveBoxArgSystem<Data, Ret> {
+                let f = Box::new(move |data: Data, world: &mut World| unsafe {
+                    S::run_arg_ex(&mut self, data, world)
+                });
+
+                ExclusiveBoxArgSystem {
+                    f,
+                }
+            }
+        }
     };
 }
 
@@ -122,5 +204,17 @@ where
         let f = Box::new(move |world: &mut World| unsafe { S::run_ex(&mut self, world) });
 
         ExclusiveBoxSystem { f }
+    }
+}
+
+impl<Data, S, Ret> IntoExclusiveBoxArgSystem<Data, World, Ret> for S
+where
+    S: ExclusiveArgSystem<Data, World, Ret> + 'static,
+{
+    fn into_ex_box_arg_system(mut self) -> ExclusiveBoxArgSystem<Data, Ret> {
+        let f = Box::new(move |data: Data, world: &mut World| unsafe {
+            S::run_arg_ex(&mut self, data, world)
+        });
+        ExclusiveBoxArgSystem { f }
     }
 }
